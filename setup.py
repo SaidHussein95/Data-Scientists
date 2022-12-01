11526f9feebe788d7bccfd89f985f9205fa1917f
hussein said <hussein.said@fau.de>
2022-12-01 18:53:24 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="snowflake",
+    version="0.1",
+    author="hussein said",
+    author_email="hussein.said@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy","turtles"],
+
+)
